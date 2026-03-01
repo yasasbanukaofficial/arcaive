f2ab92f08fcfebdc429615abdb89996ae8ec1888
yasas banu <yixbinfo@gmail.com>
2026-03-02 01:50:14 +0530

    feat(member): refactor member settings hook to use customerAPI for data fetching

---
+++ frontend/src/features/settings/api/customerAPI.ts
@@ -0,0 +1,7 @@
+import { apiInstance } from "@/api/api";
+
+const MEMBER_DATA_URL = `${process.env.NEXT_PUBLIC_API_URL}/members/${process.env.NEXT_PUBLIC_MEMBER_ID}`!;
+
+export const customerAPI = {
+  get: async() => (await apiInstance({ baseURL: MEMBER_DATA_URL})).data.data
+};
--- frontend/src/features/settings/hooks/useMember.ts
+++ frontend/src/features/settings/hooks/useMember.ts
@@ -1,12 +1,6 @@
 "use client";
-import { apiInstance } from "@/api/api";
 import { useQuery } from "@tanstack/react-query";
-
-const MEMBER_DATA_URL = `${process.env.NEXT_PUBLIC_API_URL}/members/${process.env.NEXT_PUBLIC_MEMBER_ID}`!;
-
-const customerAPI = {
-  get: async() => (await apiInstance({ baseURL: MEMBER_DATA_URL})).data.data
-};
+import { customerAPI } from "../api/customerAPI";
 
 export function useMemberSettings() {
   return useQuery({
